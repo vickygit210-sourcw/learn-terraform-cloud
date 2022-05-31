787a2ebde51db20def632a08e7733510a3a73f26
vicky <vicky210@gmail.com>
2022-05-31 15:37:37 +0530

    Removed cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "bikrampersonal"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
